8725e398fa9562f1543e2aa99002d1a0e64dbccd
Adewunmi Oladele <rkutalian@gmail.com>
2023-06-13 15:16:41 +0000

    fix ALLOWED_HOSTS

--- phoenixmart/settings.py
+++ phoenixmart/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = ('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['phoenixmart.onrender.com']
 
 
 # Application definition

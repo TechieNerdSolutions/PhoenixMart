25d3f3592fb77329e6eeddbc279ca748ceedc448
Adewunmi Oladele <rkutalian@gmail.com>
2023-06-14 10:25:43 +0000

    collect statiifiles

--- phoenixmart/settings.py
+++ phoenixmart/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY ='SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['phoenixmart.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

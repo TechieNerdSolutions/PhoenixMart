0f20b60c43258bdd9bfab9cc90aada2d8bbe549f
Adewunmi Oladele <rkutalian@gmail.com>
2023-06-22 00:45:56 +0000

    initial commit

--- phoenixmart/settings.py
+++ phoenixmart/settings.py
@@ -102,15 +102,15 @@ WSGI_APPLICATION = 'phoenixmart.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
 }
 
-# DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-# }
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 
 # Password validation

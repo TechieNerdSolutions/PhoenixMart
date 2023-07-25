757a4d36ae2e15fd58385a41d48e075c146c82c4
Adewunmi Oladele <rkutalian@gmail.com>
2023-07-25 04:40:19 +0000

    Bad HTML filtering regexp

--- staticfiles/ckeditor/galleriffic/js/jush.js
+++ staticfiles/ckeditor/galleriffic/js/jush.js
@@ -97,7 +97,7 @@ var jush = {
 		var num = /(?:\b[0-9]+\.?[0-9]*|\.[0-9]+)(?:[eE][+-]?[0-9]+)?/;
 		var tr = { // transitions
 			htm: { php: php, tag_css: /(<)(style)\b/i, tag_js: /(<)(script)\b/i, htm_com: /<!--/, 0: /(<!)([^>]*)(>)/, tag: /(<)([^<>\s]+)/, ent: /&/ },
-			htm_com: { php: php, 1: /-->/ },
+			htm_com: { php: php, 1: /(<!--|-!)([^>]*)(-->|-!>)/ },
 			ent: { php: php, 1: /;/ },
 			tag: { php: php, att_css: /(\s+)(style)(\s*=\s*)/i, att_js: /(\s+)(on[^=<>\s]+)(\s*=\s*)/i, att: /(\s+)([^=<>\s]*)(\s*)/, 1: />/ },
 			tag_css: { php: php, att: /(\s+)([^=<>\s]*)(\s*)/, css: />/ },
